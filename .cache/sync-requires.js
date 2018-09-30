c5576ad52d29aebac25051892a81f79a22923b74
Andrea <andrea.a.finlay@gmail.com>
2018-09-30 12:24:41 -0400

    prepare test deploy

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,7 +3,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/Andrea/Documents/portfolio/pith/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/Andrea/Documents/portfolio/pith/src/pages/404.js")),
   "component---src-pages-about-js": preferDefault(require("/Users/Andrea/Documents/portfolio/pith/src/pages/about.js")),
   "component---src-pages-contact-js": preferDefault(require("/Users/Andrea/Documents/portfolio/pith/src/pages/contact.js")),
---
+++ public/component---src-pages-about-js-e75765c95e2b067e7f7f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{136:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a),c=t(141);e.default=function(){return u.a.createElement(c.a,null)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-e75765c95e2b067e7f7f.js.map
