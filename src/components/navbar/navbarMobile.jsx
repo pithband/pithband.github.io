932581365784f8c377f40ec41ce0962391d1e800
Andrea <andrea.a.finlay@gmail.com>
2018-10-01 01:03:13 -0400

    modify burger

--- src/components/navbar/navbarMobile.jsx
+++ src/components/navbar/navbarMobile.jsx
@@ -3,10 +3,20 @@ import Link from 'gatsby-link';
 
 const handleClick = function(e) {
     const dropdown = document.querySelector('#dropdown');
+    const burgerWrapper = document.querySelector('#burger-wrapper');
+    const burger = document.querySelector('#burger');
+    const line1 = document.querySelector('#line1');
+    const line2 = document.querySelector('#line2');
+    const line3 = document.querySelector('#line3');
 
 	e.preventDefault();
 	dropdown.classList.toggle('xs:flex');
-	dropdown.classList.toggle('xs:hidden');
+    dropdown.classList.toggle('xs:hidden');
+    burgerWrapper.classList.toggle('open');
+    burger.classList.toggle('border-bud');
+    line1.classList.toggle('bg-bud');
+    line2.classList.toggle('bg-bud');
+    line3.classList.toggle('bg-bud');
 };
 
 const NavBarMobile = () => (
@@ -45,15 +55,14 @@ const NavBarMobile = () => (
 			</div>
 
             <button
-                className="block lg:hidden shadow-outline flex items-center px-3 py-2 mr-4 rounded text-gunmetal"
+                id="burger"
+                className="block focus:outline-none lg:hidden border-3 flex items-center px-2 py-1 mr-4 rounded border-gunmetal"
                 onClick={handleClick}>
-                <svg
-                    className="fill-current h-3 w-3"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <title>Menu</title>
-                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                </svg>
+                <div id="burger-wrapper" className="h-6 flex flex-col justify-between">
+                    <div id="line1" className="line half start w-3 h-1 bg-gunmetal rounded" />
+                    <div id="line2" className="line w-6 h-1 bg-gunmetal rounded" />
+                    <div id="line3" className="line half end self-end w-3 h-1 bg-gunmetal rounded" />
+                </div>
             </button>
 
 		</div>
